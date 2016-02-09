57f880550d8dff8429c48b9a54f4ea431cec19b4
Miles Hochstein <miles.hochstein@gmail.com>
2016-02-09 13:58:25 -0800

    refactor

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(( req, res, next ) => {
+    const url = '*';
+
+    res.header('Access-Control-Allow-Origin', url);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
+app.get('/api/monkeys', function (req, res) {
+    res.send('good morning zoo ');
+});
+
+
+
+console.log("listening 3000");
+app.listen(3000);
