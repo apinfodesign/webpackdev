72f36bdc1c18064a03ce4cfc89433648b3a2257e
Miles Hochstein <miles.hochstein@gmail.com>
2016-02-19 15:57:41 -0800

    two working tests against in range out of range age

--- app/test-e2e/formProtractorTest.js
+++ app/test-e2e/formProtractorTest.js
@@ -22,19 +22,17 @@ describe('magazine creator form', function() {
 
     });
 
+
     it('should display ERROR  msg for out of range age ', function() {
-
-            //find css for age input and clear value
+         //find css for age input and clear value
             element(by.css('input[name=age]')).clear();
-
-            //insert non valid age 121
+        //insert non valid age 121
             element(by.css('input[name=age]')).sendKeys('121')
-
-            //get display value for error msg
+        //get display value for error msg
             var displayValue = element(by.css('input[name=age]')).element(by.xpath("..")).element(by.tagName('span')).getCssValue('display')
-
-
         expect(displayValue).toEqual('inline');
         });
 
 });
+
+
